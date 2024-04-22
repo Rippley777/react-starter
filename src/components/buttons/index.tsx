fc3f7d15b6880efd5142946371ede091246d0c9b
Ally Rippley <ally.rippley@gmail.com>
2024-04-22 12:59:04 -0500

    fix button not working

--- src/components/buttons/index.tsx
+++ src/components/buttons/index.tsx
@@ -3,9 +3,9 @@ interface CompProps extends React.ButtonHTMLAttributes<HTMLButtonElement> {
     children: React.ReactNode;
 };
 
-const Button = ({ children }: CompProps) => {
+const Button = ({ children, ...props }: CompProps) => {
     return (
-        <button className="py-2 px-4 rounded-full text-white m-5 bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300" type="submit">{children}</button>
+        <button {...props} className="py-2 px-4 rounded-full text-white m-5 bg-violet-500 hover:bg-violet-600 active:bg-violet-700 focus:outline-none focus:ring focus:ring-violet-300" type="submit">{children}</button>
     );
 };
 
