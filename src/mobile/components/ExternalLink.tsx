8053116c333dfd0fd7b37949386f93f162615ad5
Ally Rippley <ally.rippley@gmail.com>
2024-04-24 00:36:47 -0500

    desperate times... 🤠

--- src/mobile/components/ExternalLink.tsx
+++ src/mobile/components/ExternalLink.tsx
@@ -2,7 +2,6 @@ import { Link } from 'expo-router';
 import * as WebBrowser from 'expo-web-browser';
 import React from 'react';
 import { Platform } from 'react-native';
-import { formatHref } from '../utils';
 
 export function ExternalLink(
   props: Omit<React.ComponentProps<typeof Link>, 'href'> & { href: string }
@@ -12,7 +11,8 @@ export function ExternalLink(
     <Link
       target="_blank"
       {...props}
-      href={formatHref(props.href)}
+      // @ts-ignore
+      href={props.href}
       onPress={(e) => {
         if (Platform.OS !== 'web') {
           // Prevent the default behavior of linking to the default browser on native.
